4a20cf3f3293b20f59bbde71d838b41008db2811
Md.Dilshadul Islam <mddilshadul98@gmail.com>
2023-04-28 06:51:12 +0600

    Updates

---
+++ precache-manifest.373870ee10ab579424e6633c4dbd6d5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c886b3dd8ee0b9e27772",
+    "url": "/ecommerce/static/css/main.41594a70.chunk.css"
+  },
+  {
+    "revision": "c886b3dd8ee0b9e27772",
+    "url": "/ecommerce/static/js/main.c886b3dd.chunk.js"
+  },
+  {
+    "revision": "ec5ddaa9f6f0a593122a",
+    "url": "/ecommerce/static/js/1.ec5ddaa9.chunk.js"
+  },
+  {
+    "revision": "453927826d4b7f625288",
+    "url": "/ecommerce/static/css/2.b7c6fe30.chunk.css"
+  },
+  {
+    "revision": "453927826d4b7f625288",
+    "url": "/ecommerce/static/js/2.45392782.chunk.js"
+  },
+  {
+    "revision": "64cf79bc81c348084969",
+    "url": "/ecommerce/static/js/runtime~main.64cf79bc.js"
+  },
+  {
+    "revision": "59b09feea87d05e6a159c17172d8816c",
+    "url": "/ecommerce/index.html"
+  }
+];
